693a9adab443e4f75d9af30ce3f9ef641758f66f
Horace Nelson <horace.nelson@msg.com>
2021-05-27 21:46:26 -0400

    Remove commented out bash code

--- bin/build.sh
+++ bin/build.sh
@@ -2,6 +2,3 @@
 #build.sh
 
 sam build --use-container
-# docker build --tag amazonlinux:nodejs .
-# docker run --rm --volume ${PWD}/src/functions/:/build amazonlinux:nodejs /bin/bash -c "source ~/.bashrc; npm install querystring --save; npm install --only=prod"
-# docker run --rm --volume ${PWD}/src/functions/GetOrCreateImage:/build amazonlinux:nodejs /bin/bash -c "source ~/.bashrc; npm install sharp --save; npm install querystring --save; npm install --only=prod"
